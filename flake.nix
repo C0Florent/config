779f10bd391380fd40ec62286f5bc74724a73305
Florent Charpentier <florent.charpentier@epitech.eu>
2025-02-12 10:04:02 +0100

    :sparkles: Added additional input to stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,19 +2,24 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-stable, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
       ];
+
+      specialArgs = {
+        pkgs-stable = nixpkgs-stable.legacyPackages.${system};
+      };
     };
   };
 }
