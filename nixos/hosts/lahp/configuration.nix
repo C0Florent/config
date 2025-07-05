ca9a47625a04e7226bf978bcf14b0611e5787beb
Florent Charpentier <florent.charpentier@epitech.eu>
2025-07-05 20:21:59 +0200

    :recycle: Add automatic host discovery with `mylib.readNixOSHosts`

--- nixos/hosts/lahp/configuration.nix
+++ nixos/hosts/lahp/configuration.nix
@@ -9,8 +9,8 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
 
-      ./hyprland.nix
-      ./registry.nix
+      ../../hyprland.nix
+      ../../registry.nix
     ];
 
   # Bootloader.
