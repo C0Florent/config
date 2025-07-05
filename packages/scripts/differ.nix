7c0bd3b18053185717f1d1ebd571e93776390fcc
Florent Charpentier <florent.charpentier@epitech.eu>
2025-07-06 01:18:03 +0200

    :recycle: differ: Use `$'...'` instead of `echo -e`
    Just use a feature of bash strings instead of a flag for echo

--- packages/scripts/differ.nix
+++ packages/scripts/differ.nix
@@ -8,15 +8,15 @@ in
 pkgs.writeShellScriptBin "differ" ''
     cd /tmp
 
-    echo -ne "Put \e[31mactual\e[0m output in clipboard and press enter..."
+    echo -n $'Put \e[31mactual\e[0m output in clipboard and press enter...'
     read
     ${wl-paste} > actual
-    echo -e "\e[3;37m copied!\e[0m"
+    echo $'\e[3;37m copied!\e[0m'
 
-    echo -ne "Put \e[32mexpected\e[0m output in clipboard and press enter..."
+    echo -n $'Put \e[32mexpected\e[0m output in clipboard and press enter...'
     read
     ${wl-paste} > expected
-    echo -e "\e[3;37m copied!\e[0m"
+    echo $'\e[3;37m copied!\e[0m'
 
     ${delta} actual expected
 ''
