ad187ddd659beca8f8fca6f631351c8271ab584a
Florent Charpentier <florent.charpentier@epitech.eu>
2025-08-08 15:40:50 +1000

    :sparkles: vscode: Add OracleDB+MongoDB extensions

--- hm/monash.nix
+++ hm/monash.nix
@@ -1,6 +1,15 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, vscode-extensions, ... }:
 
 {
+  programs.vscode = {
+    profiles.default.extensions =
+    with vscode-extensions.vscode-marketplace;
+    with pkgs.vscode-extensions; [
+      oracle.sql-developer
+      mongodb.mongodb-vscode
+    ];
+  };
+
   programs.git = {
     extraConfig.credential = {
       "https://git.infotech.monash.edu".helper = "!glab auth git-credential";
