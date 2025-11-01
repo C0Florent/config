51934111b7103cb82242147d8bb61105b7374d12
Florent Charpentier <florent.charpentier@epitech.eu>
2025-11-01 20:27:49 +1100

    :sparkles: differ: Add word and line count output

--- packages/scripts/differ.nix
+++ packages/scripts/differ.nix
@@ -3,6 +3,12 @@
 let
   delta = "${pkgs.delta}/bin/delta";
   wl-paste = "${pkgs.wl-clipboard}/bin/wl-paste";
+
+  show-copied = ''
+    lines='n/a'; words='n/a'; bytes='n/a'
+    read lines words bytes < <(wl-paste | wc -l -w -m)
+    echo $'\e[3;37m copied!\e[0m'" ($lines lines, $words words)"
+  '';
 in
 
 pkgs.writeShellScriptBin "differ" ''
@@ -11,12 +17,12 @@ pkgs.writeShellScriptBin "differ" ''
     echo -n $'Put \e[31mactual\e[0m output in clipboard and press enter...'
     read
     ${wl-paste} > actual
-    echo $'\e[3;37m copied!\e[0m'
+    ${show-copied}
 
     echo -n $'Put \e[32mexpected\e[0m output in clipboard and press enter...'
     read
     ${wl-paste} > expected
-    echo $'\e[3;37m copied!\e[0m'
+    ${show-copied}
 
     ${delta} actual expected && echo $'\n== \e[3;37mExact match!\e[0m =='
 ''
