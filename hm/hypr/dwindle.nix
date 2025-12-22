210ecf2339ccf4998340f54fe2e89f7ec5760eb7
Florent Charpentier <florent.charpentier@epitech.eu>
2025-12-22 23:53:44 +1100

    :sparkles: hypr: Add SUPER+S for dwindle togglesplit

--- hm/hypr/dwindle.nix
+++ hm/hypr/dwindle.nix
@@ -4,5 +4,14 @@
   wayland.windowManager.hyprland.settings.dwindle = {
     force_split = 2;
     preserve_split = true;
+    precise_mouse_move = true;
+  };
+
+  # binds written in dwindle.nix because they are dwindle-specific
+  mycfg.hypr.superbinds.superbinds = {
+    bind = [
+      "   , s, layoutmsg, togglesplit"
+      "ALT, s, layoutmsg, swapsplit"
+    ];
   };
 }
